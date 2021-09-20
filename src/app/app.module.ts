4d86ee116ac7a2beb5f5afb898ebdad41fe95f64
upendra-stalwartdigital <58971705+upendra-stalwartdigital@users.noreply.github.com>
2021-09-20 19:05:45 +0530

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
